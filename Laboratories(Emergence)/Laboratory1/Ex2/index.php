2c7c4726687077fdd7914597b6fae7157b9d61ce
LilDarkin <70248414+LilDarkin@users.noreply.github.com>
2022-03-21 15:31:43 +0800

    CITE 006-IT32S2

---
+++ Laboratories(Emergence)/Laboratory1/Ex2/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercise 2</title>
+</head>
+
+<body>
+  
+
+    
+<?php
+   echo "<img src='http://localhost/Laboratories(Emergence)/Laboratory1/Ex2/1.png'>";
+   echo "<img src='http://localhost/Laboratories(Emergence)/Laboratory1/Ex2/2.png'>";
+   echo "<img src='http://localhost/Laboratories(Emergence)/Laboratory1/Ex2/3.png'>";
+   echo "<img src='http://localhost/Laboratories(Emergence)/Laboratory1/Ex2/4.png'>";
+   echo "<img src='http://localhost/Laboratories(Emergence)/Laboratory1/Ex2/5.png'>";
+   echo "<img src='http://localhost/Laboratories(Emergence)/Laboratory1/Ex2/6.png'>";
+   echo "<img src='http://localhost/Laboratories(Emergence)/Laboratory1/Ex2/7.png'>";
+   echo "<img src='http://localhost/Laboratories(Emergence)/Laboratory1/Ex2/8.png'>";
+   echo "<img src='http://localhost/Laboratories(Emergence)/Laboratory1/Ex2/9.png'>";
+   echo "<img src='http://localhost/Laboratories(Emergence)/Laboratory1/Ex2/10.png'>";
+?>
+ 
+</body>
+
+</html>
